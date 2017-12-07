3b89979de94d97543131db0dec2ce31c10128d59
Evan Kaplan <ekaplan6@gatech.edu>
2017-12-06 19:46:03 -0500

    auto deploy

--- main_test.py
+++ main_test.py
@@ -1,18 +1,15 @@
-import main
 import unittest
+from main import app
 
 class MainTest(unittest.TestCase):
     """This class uses the Flask tests app to run an integration test against a
     local instance of the server."""
 
     def setUp(self):
-        self.app = main.app.test_client()
+        self.app = app.test_client()
 
     def test1(self):
         self.assertEqual(1, 1)
-
-    def test2(self):
-        self.assertEqual(2, 2)
 
 if __name__ == '__main__':
     unittest.main()
